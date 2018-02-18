0215ba8acdfba6f4c6d99d45caf76a10908c814d
carolyoon <carolyoon731@gmail.com>
2018-02-18 13:19:52 -0800

    Write create method

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -3,7 +3,14 @@ class EntriesController < ApplicationController
   end
 
   def create
-    render plain: params[:entry].inspect
+    @entry = Entry.new(params.require[:article].permit(:date, :text))
+
+    if @entry.save
+      redirect_to @entry
+    else
+      @errors = entry.errors.full_messages
+      redirect_to new_entry
+    end
   end
 end
  
