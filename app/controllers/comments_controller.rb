acc440c596166ebec2dee84ad22e9615a60b1d90
carolyoon <carolyoon731@gmail.com>
2018-02-18 16:31:49 -0800

    Fix typo in comment association

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,13 @@ class CommentsController < ApplicationController
   def create
     @entry = Entry.find(params[:entry_id])
     @comment = @entry.comments.create(comment_params)
-    redirect_to entry_path(@entry)
+    
+    if @comment.save
+      redirect_to entry_path(@entry)
+    else
+      @errors = @comment.errors.full_messages
+      redirect_to entry_path(@entry)
+    end
   end
 
   private
--- db/migrate/20180218234331_create_comments.rb
+++ db/migrate/20180218234331_create_comments.rb
@@ -3,7 +3,7 @@ class CreateComments < ActiveRecord::Migration[5.1]
     create_table :comments do |t|
       t.string :commenter
       t.text :body
-      t.references :entry, foreign_key: true
+      t.references :entry
 
       t.timestamps
     end
